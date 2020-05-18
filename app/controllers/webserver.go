7bd7279c4864dd430034bb91f49c2694be870c66
hiro-511 <hiroki.anzai@tabi-labo.com>
2020-05-18 23:46:31 +0900

    add sma/ema/bbands

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -69,6 +69,65 @@ func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
 
 	df, _ := models.GetAllCandle(productCode, durationTime, limit)
 
+	sma := r.URL.Query().Get("sma")
+	if sma != "" {
+		strSmaPeriod1 := r.URL.Query().Get("smaPeriod1")
+		strSmaPeriod2 := r.URL.Query().Get("smaPeriod2")
+		strSmaPeriod3 := r.URL.Query().Get("smaPeriod3")
+		period1, err := strconv.Atoi(strSmaPeriod1)
+		if strSmaPeriod1 == "" || err != nil || period1 < 0 {
+			period1 = 7
+		}
+		period2, err := strconv.Atoi(strSmaPeriod2)
+		if strSmaPeriod2 == "" || err != nil || period2 < 0 {
+			period2 = 14
+		}
+		period3, err := strconv.Atoi(strSmaPeriod3)
+		if strSmaPeriod3 == "" || err != nil || period3 < 0 {
+			period3 = 50
+		}
+		df.AddSma(period1)
+		df.AddSma(period2)
+		df.AddSma(period3)
+	}
+
+	ema := r.URL.Query().Get("ema")
+	if ema != "" {
+		strEmaPeriod1 := r.URL.Query().Get("emaPeriod1")
+		strEmaPeriod2 := r.URL.Query().Get("emaPeriod2")
+		strEmaPeriod3 := r.URL.Query().Get("emaPeriod3")
+		period1, err := strconv.Atoi(strEmaPeriod1)
+		if strEmaPeriod1 == "" || err != nil || period1 < 0 {
+			period1 = 7
+		}
+		period2, err := strconv.Atoi(strEmaPeriod2)
+		if strEmaPeriod2 == "" || err != nil || period2 < 0 {
+			period2 = 14
+		}
+		period3, err := strconv.Atoi(strEmaPeriod3)
+		if strEmaPeriod3 == "" || err != nil || period3 < 0 {
+			period3 = 50
+		}
+		df.AddEma(period1)
+		df.AddEma(period2)
+		df.AddEma(period3)
+	}
+
+	bbands := r.URL.Query().Get("bbands")
+	if bbands != "" {
+		strN := r.URL.Query().Get("bbandsN")
+		strK := r.URL.Query().Get("bbandsK")
+		n, err := strconv.Atoi(strN)
+		if strN == "" || err != nil || n < 0 {
+			n = 20
+		}
+		k, err := strconv.Atoi(strK)
+		if strK == "" || err != nil || k < 0 {
+			k = 2
+		}
+		df.AddBBands(n, float64(k))
+	}
+
 	js, err := json.Marshal(df)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/gorilla/websocket v1.4.2
 	github.com/labstack/gommon v0.3.0
+	github.com/markcheno/go-talib v0.0.0-20190307022042-cd53a9264d70
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	gopkg.in/ini.v1 v1.55.0
 )
